f8f4b96df5c9cb0b4ed869232f6fc7c1d1870ba7
Klaus Schnitzlein <linoth@arcor.de>
2025-11-18 08:41:05 +0100

    setup.m added

---
+++ setup.m
@@ -0,0 +1,51 @@
+%% Setup for CRD-Toolbox
+%
+%    (c) Klaus Schnitzlein - 10.11.2025
+
+fprintf('Setting Path to %s\n',pwd);
+addpath('.');
+addpath(genpath('Functions')); 
+addpath(genpath('Icons')); 
+addpath(genpath('Data')); 
+
+fprintf('Setting Root Directory\n');
+setenv('root_CRD',pwd);
+
+fprintf('Adding Units\n');
+pm_addunit('cal',4.1868,'J');
+pm_addunit('kcal',4.1868,'kJ');
+pm_addunit('kmol',1.0e+03,'mol');
+pm_addunit('torr',1.33e-03,'bar');
+pm_addunit('mmHg',1.0e-03,'bar');
+
+fprintf('Refreshing Simulink Customizations\n');
+sl_refresh_customizations;
+
+answer = questdlg('Compilation of Library?', ...
+	'Question', ...
+	'Yes','No','No');
+switch answer
+    case 'Yes'
+      fprintf('Compiling Library\n');
+      ssc_build ChemReactorDesign
+      fprintf('Building Help\n');
+      setHelperFunction
+  case 'No'
+    fprintf('Starting Simscape\n');
+    run ChemReactorDesign_lib.slx
+end
+
+% answer = questdlg('Open Reference Manual?', ...
+% 	'Question', ...
+% 	'Yes','No','No');
+% switch answer
+%     case 'Yes'
+%       web(sprintf("%s/Reference/Html/Main.html",getenv('root_CRD')),'-browser');
+%   case 'No'
+% end
+
+% fprintf('Adding Composite Libraries\n');
+% addBlockToLibrary('Gas','Composite/Subsystems Gas',40,100)
+% addBlockToLibrary('Liquid','Composite/Subsystems Liquid',180,100)
+% addBlockToLibrary('Solid','Composite/Subsystems Solid',320,100)
+% addBlockToLibrary('Interface','Composite/Subsystems Interface',460,100
